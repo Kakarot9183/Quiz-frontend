f230c0aa86bf01669f0f08f17601e2584b3cadde
Aditya_Batti <adityabatti9183@gmail.com>
2024-06-22 14:36:08 +0530

    login page created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
-import Welcome from './components/Welcome';
+
 
 function App() {
   return (
     <>
       <Login />
-      <Welcome />
+      
     </>
       
     
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,36 @@
+import './Login.css'
 function Login(){
     return (
         <>
-        <h1 className="abc">
-            hello aditya
-            
-        </h1>
+        <div class="card">
+            <div class="header">
+                <h3>login</h3>
+            </div>
+            <div class="form-floating mb-3">
+                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                <label for="floatingInput">Email address</label>
+                </div>
+                <div class="form-floating">
+                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                <label for="floatingPassword">Password</label>
+                </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                <label class="form-check-label" for="flexCheckDefault">
+                    remember me
+                </label>
+            </div>
+            <div  class="button">
+                <button type="button" class="btn btn-dark">Login</button>
+            </div>
+
+        </div>
+        
+
+
+        
+        
+        
         </>
     )
 }
